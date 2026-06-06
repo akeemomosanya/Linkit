e2d4decdcd6a4587d214bec7a86d27764165abc3
adewaleomosanya <akeemomosanya98@gmail.com>
2026-06-06 17:43:34 +0100

    made changes to database.py

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine("postgresql://linkit_user:RU5oHWe4lKO2ehQV7EsIbiMWyWRz894P@dpg-d8i4b2km0tmc73ca22ig-a/linkit")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
